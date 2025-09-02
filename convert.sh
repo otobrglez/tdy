5b8af0394244a69da8cd13cc6ba612e9f1216b16
Oto Brglez <otobrglez@gmail.com>
2025-09-02 10:27:37 +0200

    Convert patched.

--- convert.sh
+++ convert.sh
@@ -4,21 +4,25 @@ set -ex
 from_file=$1
 to_file=$2
 
+font_0="inconsolata"
+font_1="Palatino"
+font_2="Menlo"
+font_3="Helvetica"
+
+font=$font_0
+mainfont=$font
+sansfont=$font
+monofont=$font
+
 pandoc $from_file \
+    --pdf-engine=xelatex \
 		-V geometry:a4paper,margin=2cm \
-		-V fontsize=12pt \
-		--variable mainfont="Palatino" \
-		--variable sansfont="Helvetica" \
-		--variable monofont="Menlo" \
-		--variable fontsize=12pt \
+    --variable mainfont="$mainfont" \
+    --variable sansfont="$sansfont" \
+    --variable monofont="$monofont" \
+    -V mainfontoptions="FallbackFonts={Symbola}" \
+    -V sansfontoptions="FallbackFonts={Symbola}" \
+    -V monofontoptions="FallbackFonts={Symbola}" \
+		--variable fontsize=10pt \
 		--variable version=2.0 \
 		-o $to_file
-
-# --variable "geometry=margin=1.2in" 
-#		#-V "documentclass:extreport" \
-#		-V 'mainfont:sans-serif' \
-
-# 		-V 'fontfamily:Iosevka' \
-#		-V 'sansfont:Iosevka' \
-#		-V 'monofont:Iosevka' \
-#		-V 'mathfont:TeXGyreDejaVuMath' \
--- devenv.nix
+++ devenv.nix
@@ -4,7 +4,21 @@
   env.NIX_ENFORCE_PURITY = 0;
   env.RUST_BACKTRACE = "full";
 
-  packages = [ pkgs.git ];
+  packages = [ 
+    pkgs.git
+    pkgs.pandoc
+
+    # https://github.com/Ptival/config/blob/bd89aed366de07c8ec683b8aefbd84cc21312519/nixos/nixpkgs/texlive.nix#L34
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive) scheme-small soul dejavu
+      collection-xetex fncychap titlesec tabulary varwidth multirow
+      hanging adjustbox collectbox stackengine sectsty tocloft
+      newunicodechar etoc framed capt-of wrapfig needspace dejavu-otf helvetic upquote
+      xcolor xifthen ifmtarg datetime extsizes memorygraphs currvita cbfonts xetex xstring
+      inconsolata lato latexmk lm lualatex-math xurl twemojis
+      ;
+    })
+  ];
 
   languages.rust = {
     enable = true;
